48d4dfff1d58c2520efa5427cf7fdba08aa822ef
Lucas Ramos <lucas_ramos95@outlook.com>
2022-09-06 16:29:18 -0300

    5th - Lista de jobs aumentada

--- src/componentes/main/index.js
+++ src/componentes/main/index.js
@@ -7,7 +7,7 @@ const Main = (props) => {
                 <div>
                     <img src='/imagens/photosnap.svg' />
                     <p>Photosnap</p>
-                    <h1>Desenvolvedor Senior Frontend</h1>
+                    <h1>Desenvolvedor Senior Front-end</h1>
                     <p>1 Dia atrás - Tempo Integral - Remoto</p>
                 </div>
             </div>
@@ -23,7 +23,7 @@ const Main = (props) => {
                 <div>
                     <img src='/imagens/account.svg' />
                     <p>Account</p>
-                    <h1>Desenvolvedor Frontend Junior</h1>
+                    <h1>Desenvolvedor Front-end Junior</h1>
                     <p>2 Dias atrás - Tempo Integral - Presencial</p>
                 </div>
             </div>
@@ -31,8 +31,56 @@ const Main = (props) => {
                 <div>
                     <img src='/imagens/myhome.svg' />
                     <p>MyHome</p>
-                    <h1>Desenvolvedor Frontend Junior</h1>
+                    <h1>Desenvolvedor Front-end Junior</h1>
                     <p>5 Dias atrás - Tempo Integral - Internacional/EUA</p>
+                </div>
+            </div>
+            <div className='listadejobs'>
+                <div>
+                    <img src='/imagens/loop-studios.svg' />
+                    <p>Loop Studios</p>
+                    <h1>Engenheiro de Software</h1>
+                    <p>5 Dias atrás - Tempo Integral - Internacional/Canadá</p>
+                </div>
+            </div>
+            <div className='listadejobs'>
+                <div>
+                    <img src='/imagens/faceit.svg' />
+                    <p>FaceIt</p>
+                    <h1>Desenvolvedor Back-end Junior</h1>
+                    <p>7 Dias atrás - Tempo Integral - Internacional/Remoto</p>
+                </div>
+            </div>
+            <div className='listadejobs'>
+                <div>
+                    <img src='/imagens/shortly.svg' />
+                    <p>Shortly</p>
+                    <h1>Desenvolvedor Junior</h1>
+                    <p>14 Dias atrás - Tempo Integral - Remoto</p>
+                </div>
+            </div>
+            <div className='listadejobs'>
+                <div>
+                    <img src='/imagens/insure.svg' />
+                    <p>Insure</p>
+                    <h1>Desenvolvedor Junior</h1>
+                    <p>14 Dias atrás - Tempo Integral - Remoto</p>
+                </div>
+            </div>
+            <div className='listadejobs'>
+                <div>
+                    <img src='/imagens/eyecam-co.svg' />
+                    <p>Eyecam Co.</p>
+                    <h1>Desenvolver Fullstack</h1>
+                    <p>17 Dias atrás - Tempo Integral - Internacional/Remoto</p>
+                </div>
+            </div>
+            <div className='listadejobs'>
+                <div>
+                    <img src='/imagens/the-air-filter-company.svg' />
+                    <p>The Air Filter Company</p>
+                    <h1>Desenvolver Front-end</h1>
+                    <p>1 Mês atrás - Meio Turno - Internacional/Remoto</p>
                 </div>
             </div>
         </>
